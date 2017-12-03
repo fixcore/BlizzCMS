e7f54b87e2a3b94c7bc8af755320196db890da6c
DZywolf <luis.wolf.core@gmail.com>
2017-12-03 18:26:58 -0300

    Remove TAB and White space (Install)

--- install/settings.php
+++ install/settings.php
@@ -2,87 +2,81 @@
 
 function connect($website_host, $website_user, $website_pass, $website_dbweb)
 {
-	$fixcore = new mysqli($website_host, $website_user, $website_pass, $website_dbweb);
-		if(mysqli_connect_error())
-			die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
-	return $fixcore;
+    $fixcore = new mysqli($website_host, $website_user, $website_pass, $website_dbweb);
+    if (mysqli_connect_error())
+        die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+    return $fixcore;
 }
 
 function SplitSQL($fixcore, $file, $delimiter = ';')
-	{
-	set_time_limit(0);
+{
+    set_time_limit(0);
 
-	if(is_file($file) === true)
-	{
-		$file = fopen($file, 'r');
+    if (is_file($file) === true)
+    {
+        $file = fopen($file, 'r');
 
-		if(is_resource($file) === true)
-		{
-			$query = array();
+        if (is_resource($file) === true)
+        {
+            $query = array();
 
-			while(feof($file) === false)
-			{
-				$query[] = fgets($file);
+            while(feof($file) === false)
+            {
+                $query[] = fgets($file);
 
-				if(preg_match('~' . preg_quote($delimiter, '~') . '\s*$~iS', end($query)) === 1)
-				{
-					$query = trim(implode('', $query));
+                if (preg_match('~' . preg_quote($delimiter, '~') . '\s*$~iS', end($query)) === 1)
+                {
+                    $query = trim(implode('', $query));
 
-					if(!$fixcore->query($query))
-						die($fixcore->error);
+                    if (!$fixcore->query($query))
+                        die($fixcore->error);
 
-					while(ob_get_level() > 0)
-					{
-						ob_end_flush();
-					}
+                    while(ob_get_level() > 0)
+                    {
+                        ob_end_flush();
+                    }
 
-					flush();
-				}
+                    flush();
+                }
 
-				if(is_string($query) === true)
-				{
-					$query = array();
-				}
-			}
-
-			return fclose($file);
-		}
-	}
-
-	return false;
+                if (is_string($query) === true)
+                {
+                    $query = array();
+                }
+            }
+            return fclose($file);
+        }
+    }
+    return false;
 }
-
 
 class Install
 {
-	private $fixcore;
+    private $fixcore;
 
-	public function __construct()
-	{
-	}
+    public function __construct() { }
 
-	private function check()
-	{
-		$folder = $_GET['test'];
+    private function check()
+    {
+        $folder = $_GET['test'];
 
-		$file = fopen("../application/".$folder."/write_test.txt", "w");
+        $file = fopen("../application/".$folder."/write_test.txt", "w");
 
-		fwrite($file, "success");
-		fclose($file);
+        fwrite($file, "success");
+        fclose($file);
 
-		unlink("../application/".$folder."/write_test.txt");
+        unlink("../application/".$folder."/write_test.txt");
 
-		die("1");
-	}
+        die("1");
+    }
 
-	private function connect()
-	{
-		$fixcore = new mysqli($website_host, $website_user, $website_pass, $website_dbweb);
-			if(mysqli_connect_error())
-				die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
-		return $fixcore;
-	}
-
+    private function connect()
+    {
+        $fixcore = new mysqli($website_host, $website_user, $website_pass, $website_dbweb);
+        if (mysqli_connect_error())
+            die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+        return $fixcore;
+    }
 }
 
 $install = new Install();
