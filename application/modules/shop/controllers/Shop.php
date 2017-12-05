cb8d30c294642a6cb0064661d9ae3d6828defe66
DZywolf <luis.wolf.core@gmail.com>
2017-12-05 00:13:10 -0300

    Remove TAB and White space (Shop)

--- application/modules/shop/controllers/Shop.php
+++ application/modules/shop/controllers/Shop.php
@@ -6,22 +6,22 @@ class Shop extends MX_Controller {
     public function index()
     {
         $this->load->model('shop_model');
-		
+
         $this->load->view('index');
         $this->load->view('footer');
     }
 
     public function addtocart()
     {
-    	$this->load->model('shop_model');
+        $this->load->model('shop_model');
 
         if ($this->m_data->isLogged() == FALSE)
             redirect(base_url('login'),'refresh');
 
         if(isset($_GET['id']) && isset($_GET['tp']))
         {
-        	$mode = $_GET['tp'];
-        	$item = $_GET['id'];
+            $mode = $_GET['tp'];
+            $item = $_GET['id'];
 
             if ($mode == "vp")
                 $this->shop_model->getVPTrue($item);
@@ -29,16 +29,15 @@ class Shop extends MX_Controller {
             if ($mode == "dp")
                 $this->shop_model->getDPTrue($item);
 
-            $this->shop_model->addtoCar(
-                $this->shop_model->getItem($item), 
-                $this->shop_model->getType($item), 
-                $this->shop_model->getPriceType($item, $mode), 
-                $mode, 
+                $this->shop_model->addtoCar(
+                $this->shop_model->getItem($item),
+                $this->shop_model->getType($item),
+                $this->shop_model->getPriceType($item, $mode),
+                $mode,
                 $this->shop_model->getQuery($item)
             );
-
         }
         else
-        	redirect(base_url('shop'),'refresh');
+            redirect(base_url('shop'),'refresh');
     }
 }
