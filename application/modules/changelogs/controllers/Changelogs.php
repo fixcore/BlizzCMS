06a810dea66b1dc19178f3dc11014e51c6dd3c49
DZywolf <luis.wolf.core@gmail.com>
2017-12-05 00:12:50 -0300

    Remove TAB and White space (Changelogs)

--- application/modules/changelogs/controllers/Changelogs.php
+++ application/modules/changelogs/controllers/Changelogs.php
@@ -3,24 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Changelogs extends MX_Controller {
 
-	public function index()
-	{
-		$this->load->model('changelogs_model');
+    public function index()
+    {
+        $this->load->model('changelogs_model');
 
-		$this->load->view('index');
-		$this->load->view('footer');
-	}
+        $this->load->view('index');
+        $this->load->view('footer');
+    }
 
-	public function id($id)
-	{
-		if (empty($id) || is_null($id))
-			redirect(base_url(),'refresh');
+    public function id($id)
+    {
+        if (empty($id) || is_null($id))
+            redirect(base_url(),'refresh');
 
-		$this->load->model('changelogs_model');
+        $this->load->model('changelogs_model');
 
-		$data['idlink'] = $id;
+        $data['idlink'] = $id;
 
-		$this->load->view('changelog', $data);
-		$this->load->view('footer');
-	}
+        $this->load->view('changelog', $data);
+        $this->load->view('footer');
+    }
 }
