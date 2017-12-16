e22869d8abf940ad3d4dbec4d865291352ec6fc6
DZywolf <luis.wolf.core@gmail.com>
2017-12-16 03:51:56 -0300

    Update PvP and Arena Modules (new UI)

--- application/modules/pvp/views/index.php
+++ application/modules/pvp/views/index.php
@@ -12,25 +12,22 @@
     <link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/main-1f799c9e0f0e26.css?v=58-88" />
     <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/images/favicon.ico">
     <!-- UiKit Start -->
-<!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/css/uikit.min.css" />
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/css/uikit.min.css" />
 
-<!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js"></script>
-<!-- UiKit end -->
-<!-- semantic ui Start -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/semanticui/semantic.min.css">
-    <!-- semantic ui End -->
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js"></script>
+    <!-- UiKit end -->
+    <!-- font-awesome Start -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- font-awesome End -->
     <!-- custom START -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/scroll.css">
+    <!--<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/scroll.css">-->
     <!-- custom END -->
 
     <!-- custom footer -->
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <!-- semantic -->
-    <script src="<?= base_url(); ?>assets/semanticui/semantic.min.js"></script>
-    <!-- semantic -->
     <!-- custom footer -->
 </head>
 
@@ -49,29 +46,29 @@
             <div class="container">
                 <!-- -->
                 <div class="space-adaptive-small"></div>
-                <h2 class="h5 flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><i class="trophy icon"></i><?= $this->m_general->getRealmName(); ?></h2>
-                <h3 class="flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><?=$this->lang->line('lad_pvp');?></h3>
+                <h2 class="h5 flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><i class="fa fa-trophy" aria-hidden="true"></i> <?= $this->m_general->getRealmName(); ?></h2>
+                <h4 class="flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><?=$this->lang->line('lad_pvp');?></h4>
                 <div class="space-adaptive-small"></div>
                 <!-- -->
-                <table class="ui selectable inverted table">
+                <table class="uk-table uk-table-responsive uk-table-divider">
                     <thead>
-                        <h3 class="flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><div class="ui purple horizontal large label">TOP 20</div></h3>
+                        <h4 class="flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><span class="uk-label uk-label-danger">TOP 20</span></h4>
                         <tr>
-                            <th><i class="user circle outline icon"></i><?=$this->lang->line('name');?></th>
-                            <th class="center aligned"><i class="flag outline icon"></i><?=$this->lang->line('faction');?></th>
-                            <th class="center aligned"><i class="crosshairs icon"></i><?=$this->lang->line('total_kills');?></th>
-                            <th class="center aligned"><i class="crosshairs icon"></i><?=$this->lang->line('today_kills');?></th>
-                            <th class="center aligned"><i class="crosshairs icon"></i><?=$this->lang->line('yersterday_kills');?></th>
+                            <th class="uk-width-small" style="color: #fff;"><i class="fa fa-user" aria-hidden="true"></i> <?=$this->lang->line('name');?></th>
+                            <th class="uk-width-small" style="color: #fff; text-align: center;"><i class="fa fa-flag" aria-hidden="true"></i> <?=$this->lang->line('faction');?></th>
+                            <th class="uk-width-small" style="color: #fff; text-align: center;"><i class="fa fa-info-circle" aria-hidden="true"></i> <?=$this->lang->line('total_kills');?></th>
+                            <th class="uk-width-small" style="color: #fff; text-align: center;"><i class="fa fa-crosshairs" aria-hidden="true"></i> <?=$this->lang->line('today_kills');?></th>
+                            <th class="uk-width-small" style="color: #fff; text-align: center;"><i class="fa fa-crosshairs" aria-hidden="true"></i> <?=$this->lang->line('yersterday_kills');?></th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php foreach ($this->pvp_model->getTop20PVP()->result() as $tops) { ?>
                             <tr>
                                 <td><?= $tops->name ?></td>
-                                <td class="center aligned"><img src="<?= base_url(); ?>assets/images/<?= $this->m_general->getFaction($tops->race) ?>.png" class="img-circle"></td>
-                                <td class="center aligned"><?= $tops->totalKills ?></td>
-                                <td class="center aligned"><?= $tops->todayKills ?></td>
-                                <td class="center aligned"><?= $tops->yesterdayKills ?></td>
+                                <td style="text-align: center;"><img src="<?= base_url(); ?>assets/images/<?= $this->m_general->getFaction($tops->race) ?>.png" class="img-circle"></td>
+                                <td style="text-align: center;"><?= $tops->totalKills ?></td>
+                                <td style="text-align: center;"><?= $tops->todayKills ?></td>
+                                <td style="text-align: center;"><?= $tops->yesterdayKills ?></td>
                             </tr>
                         <?php } ?>
                     </tbody>
