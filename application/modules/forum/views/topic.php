c3e6a81d67b37fa9e228f5f01d5f26f582268eb4
DZywolf <luis.wolf.core@gmail.com>
2017-12-20 20:42:31 -0300

    Fixed issues in Forum

--- application/modules/forum/views/topic.php
+++ application/modules/forum/views/topic.php
@@ -1,40 +1,36 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
     <title><?= $this->config->item('ProjectName'); ?> - <?= $this->lang->line('forums'); ?></title>
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="index.html" />
-	<meta property="og:title" content="Blizzard Forums" />
-	<link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/images/favicon.ico">
-	<link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/navbar0e26.css?v=58-88" />
-	<link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/main-1f799c9e0f0e26.css?v=58-88" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="index.html" />
+    <meta property="og:title" content="Blizzard Forums" />
+    <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/images/favicon.ico">
+    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/navbar0e26.css?v=58-88" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/main-1f799c9e0f0e26.css?v=58-88" />
+    <!-- UiKit Start -->
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js"></script>
+    <!-- UiKit end -->
     <!-- font-awesome Start -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- font-awesome End -->
-	<!-- UiKit Start -->
-<!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/css/uikit.min.css" />
+    <!-- custom START -->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/scroll.css">
+    <!-- custom END -->
 
-<!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js"></script>
-<!-- UiKit end -->
-	<!-- custom START -->
-	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/scroll.css">
-	<!-- custom END -->
-
-	<!-- custom footer -->
-	<script
-	  src="https://code.jquery.com/jquery-3.1.1.min.js"
-	  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-	  crossorigin="anonymous"></script>
-	<!-- custom footer -->
-	<!--[if lte IE 8]>
-		<script type="text/javascript" src="/<?= base_url(); ?>assets/js/jquery.min.js?v=88"></script>
-	<![endif]-->
-
+    <!-- custom footer -->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <!-- custom footer -->
+    <!--[if lte IE 8]>
+        <script type="text/javascript" src="/<?= base_url(); ?>assets/js/jquery.min.js?v=88"></script>
+    <![endif]-->
 </head>
 
 <body class="en-us Theme--<?= $this->m_general->getTheme(); ?> glass-header preload" lang="en" data-locale="en-gb" data-device="desktop" data-name="index">
@@ -60,9 +56,9 @@
 	
 	<div class="Breadcrumbs"> 
 		
-		<span class="Breadcrumb"> <i class="talk icon"></i>
+		<span class="Breadcrumb">
 			<a class="Breadcrumb-content"> 
-				 <?= $this->forum_model->getSpecifyPostName($idlink); ?></span>
+				 <i class="fa fa-commenting" aria-hidden="true"></i> <?= $this->forum_model->getSpecifyPostName($idlink); ?>
 			</a> 
 		</span>
 
@@ -75,11 +71,9 @@
 			<!-- logged -->
 			<?php if ($this->m_data->isLogged()) { ?>
 				    <!-- credits -->
-				    <img src="<?= base_url('assets/images/dp.jpg'); ?>" alt="" style="width: 20px; height: 20px;">
-				    <?= $this->m_general->getCharDPTotal($this->session->userdata('fx_sess_id')); ?>
+				    <img class="uk-border-circle" src="<?= base_url('assets/images/dp.jpg'); ?>" title="Donor Points" width="20px" height="20px" uk-tooltip="pos: bottom"><span class="uk-badge"><?= $this->m_general->getCharDPTotal($this->session->userdata('fx_sess_id')); ?></span>
 				     | 
-				    <img src="<?= base_url('assets/images/vp.jpg'); ?>" alt="" style="width: 20px; height: 20px;">
-				    <?= $this->m_general->getCharVPTotal($this->session->userdata('fx_sess_id')); ?>
+				    <img class="uk-border-circle" src="<?= base_url('assets/images/vp.jpg'); ?>" title="Voter Points" width="20px" height="20px" uk-tooltip="pos: bottom"><span class="uk-badge"><?= $this->m_general->getCharVPTotal($this->session->userdata('fx_sess_id')); ?></span>
 				    <!-- credits -->
 			<?php } ?>
 			<!-- logged -->
@@ -106,7 +100,7 @@
 		<header class="Topic-header">
 			<div class="Container Container--content">
 				<h1 class="Topic-heading">
-					<span class="Topic-title" data-topic-heading="true" style="color: #fff;"><i class="talk icon"></i> <?= $this->forum_model->getSpecifyPostName($idlink); ?></span>
+					<span class="Topic-title" data-topic-heading="true" style="color: #fff;"><i class="fa fa-commenting" aria-hidden="true"></i> <?= $this->forum_model->getSpecifyPostName($idlink); ?></span>
 				</h1>
 			</div>
 		</header>
@@ -182,7 +176,7 @@
 	    <div class="Author-data" data-topic-form=''>
 			<div class="LoginPlaceholder" id="create-topic">
 				<header class="LoginPlaceholder-header">
-					<h1 class="LoginPlaceholder-heading"><i class="comments outline icon"></i> <?= $this->lang->line('text_login_forumstxt'); ?></h1>
+					<h1 class="LoginPlaceholder-heading"><i class="fa fa-comments-o" aria-hidden="true"></i> <?= $this->lang->line('text_login_forumstxt'); ?></h1>
 				</header>
 				<div class="LoginPlaceholder-content">
 					<div class="LoginPlaceholder-details">
@@ -297,7 +291,7 @@
 <section xmlns="http://www.w3.org/1999/xhtml" class="Section Section--secondary">
 	<div data-topic-post="true" tabindex="0" class="TopicForm is-editing" id="topic-reply">
 	        <header class="TopicForm-header">
-	            <h1 class="TopicForm-heading"><i class="comments outline icon"></i> <?= $this->lang->line('text_login_forumstxt'); ?></h1>
+	            <h1 class="TopicForm-heading"><i class="fa fa-comments-o" aria-hidden="true"></i> <?= $this->lang->line('text_login_forumstxt'); ?></h1>
 	        </header>
 	        <div class="TopicForm-content">
 	            <aside class="TopicForm-author" data-topic-form="{&quot;userId&quot;: 207424185944    }">
@@ -332,7 +326,7 @@
 
 		            <div class="TopicForm-action--buttons">
 		            	<button type="submit" name="button_addcommentary" class="TopicForm-button TopicForm-button--reply" id="submit-button">
-		            		<span class="Button-content"><i class="fa fa-reply"></i> <?= $this->lang->line('button_addreply'); ?></span>
+		            		<span class="Button-content"><i class="fa fa-reply" aria-hidden="true"></i> <?= $this->lang->line('button_addreply'); ?></span>
 		            	</button>
 		            </div>
 		        </form>
