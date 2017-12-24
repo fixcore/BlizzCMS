952c85cb8c6423a6e3f745af73f44f78a5d6ba31
Vipo <develop.km14@gmail.com>
2017-12-24 15:43:33 +0300

    Custom Pages

---
+++ application/modules/pages/views/page.php
@@ -0,0 +1,63 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <title><?= $this->config->item('ProjectName'); ?> - <?= $this->pages_model->getName($idlink) ?></title>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="index.html" />
+    <meta property="og:title" content="Blizzard Forums" />
+    <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/images/favicon.ico">
+    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/navbar0e26.css?v=58-88" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url(); ?>assets/css/main-1f799c9e0f0e26.css?v=58-88" />
+    <!-- UiKit Start -->
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>core/uikit/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="<?= base_url(); ?>core/uikit/js/uikit.min.js"></script>
+    <script src="<?= base_url(); ?>core/uikit/js/uikit-icons.min.js"></script>
+    <!-- UiKit end -->
+    <!-- font-awesome Start -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- font-awesome End -->
+    <!-- custom START -->
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/scroll.css">
+    <!-- custom END -->
+
+    <!-- custom footer -->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <!-- custom footer -->
+    <!--[if lte IE 8]>
+        <script type="text/javascript" src="/<?= base_url(); ?>assets/js/jquery.min.js?v=88"></script>
+    <![endif]-->
+</head>
+
+<body class="en-us Theme--<?= $this->m_general->getTheme(); ?> glass-header preload" lang="en" data-locale="en-gb" data-device="desktop" data-name="index">
+
+<!-- header -->
+    <?php $this->load->view('general/icons'); ?>
+    <!-- submenu -->
+    </div>
+    </div>
+    </div>
+    <!-- submenu -->
+
+<!-- main -->
+
+<div role="main">
+    <section class="Topic" data-topic='{ "id":<?= $idlink ?>, "lastPosition":0,"forum":{"id":<?= $idlink ?>},"isSticky":true,"isFeatured":false,"isLocked":true,"isHidden":false,"isFrozen":false, "isSpam":false, "pollId":0 }' data-user='{}'>
+
+        <header class="Topic-header">
+            <div class="Container Container--content">
+                <p><?= $this->pages_model->getDesc($idlink); ?></p>
+            </div>
+        </header>
+
+        <header class="Topic-header">
+            <div class="Container Container--content">
+                <p><?= date('Y-m-d', $this->pages_model->getDate($idlink)); ?></p>
+            </div>
+        </header>
+
+    </section>
