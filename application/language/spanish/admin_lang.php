d641de5aa925d2796359ae42985281df34782c46
DZywolf <luis.wolf.core@gmail.com>
2018-01-07 16:37:11 -0300

    Add spanish language (Thanks to Rome#6070)

---
+++ application/language/spanish/admin_lang.php
@@ -0,0 +1,47 @@
+﻿<?php
+/**
+ * BlizzCMS
+ *
+ * An open source application based on Codeigniter for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['adm_home'] = 'Inicio';
+$lang['adm_panel'] = 'Panel de administración';
+$lang['adm_admin'] = 'Administrador';
+$lang['adm_nav'] = 'Navegación';
+$lang['adm_dashboard'] = 'Tablero';
+$lang['adm_users'] = 'Usuarios';
+$lang['adm_users_list'] = 'Lista de usuarios';
+$lang['adm_chars_list'] = 'Lista de personajes';
+$lang['adm_news'] = 'Noticias';
+$lang['adm_add_news'] = 'Crear noticias';
+$lang['adm_news_list'] = 'Lista de noticias';
+$lang['adm_forums'] = 'Foros';
+$lang['adm_account_settings'] = 'Configuración de la cuenta';
+$lang['adm_manageChangelog'] = 'Administrar registro de cambios';
+$lang['adm_createChangelog'] = 'Crear registro de cambios';
+$lang['adm_manageShop'] = 'Administrar Tienda';
+$lang['adm_createShop'] = 'Crear Tienda';
