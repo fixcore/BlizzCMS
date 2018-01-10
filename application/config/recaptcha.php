5103a7ce4c2e3dd24d6c0964a8b049a5b4cfbe92
Vipo <develop.km14@gmail.com>
2018-01-10 18:47:47 +0300

    docs

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -1,5 +1,5 @@
 <?php 
-
+//create here https://www.google.com/recaptcha/admin#list
 $config['recaptcha_sitekey'] = "6LfJFEAUAAAAAKBrGYQaX-TeWGBG1vTNlqUuNyNA";
 $config['recaptcha_secretkey'] = "6LfJFEAUAAAAAEbY3UgbdfmbOPd86Qs2LtnbPtXL";
 $config['lang'] = "en";
