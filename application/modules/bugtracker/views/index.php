5a15a1999ac0a76b0d3382294c5d60746b5e8f16
DZywolf <luis.wolf.core@gmail.com>
2018-01-31 09:46:19 -0300

    Rewrite Bugtracker

--- application/modules/bugtracker/views/index.php
+++ application/modules/bugtracker/views/index.php
@@ -38,120 +38,117 @@
     </div>
     </div>
     <!-- submenu -->
-    <br><br><br>
-    <div role="main">
-        <section class="Forum">
-            <header class="Forum-header">
-                <div class="Container Container--content">
+    <div class="Page-container">
+        <div class="Page-content en-US">
+            <section class="Forum">
+                <header class="Forum-header">
                     <div class="space-adaptive-medium"></div>
-                    <br></br>
-                    <h1 class="Forum-heading" style="color: #fff;"><i class="fa fa-bug" aria-hidden="true"></i> <?= $this->lang->line('nav_bugtracker'); ?></h1>
-                    <div class="Forum-controls">
-                        <?php if ($this->m_data->isLogged()) { ?>
-                            <a href="#" uk-toggle="target: #createReport">
-                                <button class="uk-button uk-button-primary">
-                                    <i class="fa fa-pencil" aria-hidden="true"></i> <?= $this->lang->line('button_create_report'); ?>
-                                </button>
-                            </a>
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="Container Container--content">
-                    <h3 class="flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><?= $this->lang->line('bugtracker_report_list'); ?></h3>
-                </div>
-            </header>
-            <div class="Forum-content" data-track="nexus.checkbox" id="forum-topics">
-                <!-- table START -->
-                <div align="right" id="pagination_link"></div>
-                <div class="table-responsive" id="bugtracker_table"></div>
-                <!-- table END -->
-
-                <script>
-                    $(document).ready(function(){
-                     function load_country_data(page)
-                     {
-                      $.ajax({
-                       url:"<?php echo base_url(); ?>bugtracker/pagination/"+page,
-                       method:"GET",
-                       dataType:"json",
-                       success:function(data)
-                       {
-                        $('#bugtracker_table').html(data.bugtracker_table);
-                        $('#pagination_link').html(data.pagination_link);
-                       }
-                      });
-                     }
-                     
-                     load_country_data(1);
-                     $(document).on("click", ".pagination li a", function(event){
-                      event.preventDefault();
-                      var page = $(this).data("ci-pagination-page");
-                      load_country_data(page);
-                     });
-                    });
-                </script>
-            </div>
-        </section>
-    </div>
-
-<div id="createReport" class="uk-modal-container" uk-modal>
-    <div class="uk-modal-dialog">
-        <button class="uk-modal-close-default" type="button" uk-close></button>
-        <div class="uk-modal-header">
-            <h2 class="uk-modal-title"><i class="fa fa-bug" aria-hidden="true"></i> <?= $this->lang->line('form_create_bug_report'); ?></h2>
-        </div>
-        <?= form_open(base_url('bugtracker/create')); ?>
-            <div class="uk-modal-body">
-                <div class="uk-margin">
-                    <label class="uk-form-label uk-text-large"><?= $this->lang->line('form_title'); ?></label>
-                    <div class="uk-form-controls">
-                        <div class="uk-inline uk-width-1-1">
-                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: pencil"></span>
-                            <?= form_input($title_from); ?>
+                    <div class="Container Container--content">
+                        <div class="space-adaptive-medium"></div>
+                        <h1 class="Forum-heading" style="color: #fff;"><i class="fa fa-bug" aria-hidden="true"></i> <?= $this->lang->line('nav_bugtracker'); ?></h1>
+                        <div class="Forum-controls">
+                            <?php if ($this->m_data->isLogged()) { ?>
+                                <a href="#" uk-toggle="target: #createReport">
+                                    <button class="uk-button uk-button-primary"><i class="fa fa-pencil" aria-hidden="true"></i> <?= $this->lang->line('button_create_report'); ?></button>
+                                </a>
+                            <?php } ?>
                         </div>
                     </div>
-                </div>
-                <div class="uk-margin">
-                    <label class="uk-form-label uk-text-large"><?= $this->lang->line('form_type'); ?></label>
-                    <div class="uk-form-controls">
-                        <!-- dropdown -->
-                        <?php 
-                            $array = array();
-                            foreach($this->bugtracker_model->getTypes() as $row ){
-                                $array[] = $row->title;
+                    <div class="Container Container--content">
+                        <h3 class="flush-bottom flush-top text-upper text-heavy" style="color: #fff;"><?= $this->lang->line('bugtracker_report_list'); ?></h3>
+                    </div>
+                </header>
+                <div class="Forum-content" data-track="nexus.checkbox" id="forum-topics">
+                    <!-- table START -->
+                    <div align="right" id="pagination_link"></div>
+                    <div class="table-responsive" id="bugtracker_table"></div>
+                    <!-- table END -->
+                    <script>
+                        $(document).ready(function(){
+                            function load_country_data(page)
+                            {
+                                $.ajax({
+                                    url:"<?php echo base_url(); ?>bugtracker/pagination/"+page,
+                                    method:"GET",
+                                    dataType:"json",
+                                    success:function(data)
+                                    {
+                                        $('#bugtracker_table').html(data.bugtracker_table);
+                                        $('#pagination_link').html(data.pagination_link);
+                                    }
+                                });
                             }
-                            echo form_dropdown('type_Bug',  $array , '', $classDrop);
-                        ?>
-                        <!-- dropdown -->
-                    </div>
+                            load_country_data(1);
+                            $(document).on("click", ".pagination li a", function(event){
+                                event.preventDefault();
+                                var page = $(this).data("ci-pagination-page");
+                                load_country_data(page);
+                            });
+                        });
+                    </script>
                 </div>
-
-                <script src="<?= base_url(); ?>core/ckeditor_basic/ckeditor.js"></script>
-
-                <div class="uk-margin">
-                    <label class="uk-form-label uk-text-large"><?= $this->lang->line('form_description'); ?></label>
-                    <div class="uk-form-controls">
-                        <div class="uk-width-1-1">
-                            <?= form_textarea('bug_description', $this->config->item('textarea'), 'id="ckeditor"'); ?>
-                            <script>
-                                CKEDITOR.replace('ckeditor');
-                            </script>
-                        </div>
-                    </div>
-                </div>
-                <div class="uk-margin">
-                    <div class="uk-form-controls">
-                        <div class="uk-inline uk-width-1-1">
-                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: link"></span>
-                            <?= form_input($url_form); ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="uk-modal-footer uk-text-right actions">
-                <?= form_button('button_cancel', $this->lang->line('button_cancel'), $close_form); ?>
-                <?= form_submit($submit_form); ?>
-            </div>
-        <?php form_close(); ?>
+            </section>
+        </div>
     </div>
-</div>
+
+    <div id="createReport" class="uk-modal-container" uk-modal>
+        <div class="uk-modal-dialog">
+            <button class="uk-modal-close-default" type="button" uk-close></button>
+            <div class="uk-modal-header">
+                <h2 class="uk-modal-title"><i class="fa fa-bug" aria-hidden="true"></i> <?= $this->lang->line('form_create_bug_report'); ?></h2>
+            </div>
+            <?= form_open(base_url('bugtracker/create')); ?>
+                <div class="uk-modal-body">
+                    <div class="uk-margin">
+                        <label class="uk-form-label uk-text-large"><?= $this->lang->line('form_title'); ?></label>
+                        <div class="uk-form-controls">
+                            <div class="uk-inline uk-width-1-1">
+                                <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: pencil"></span>
+                                <?= form_input($title_from); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <label class="uk-form-label uk-text-large"><?= $this->lang->line('form_type'); ?></label>
+                        <div class="uk-form-controls">
+                            <!-- dropdown -->
+                            <?php 
+                                $array = array();
+                                foreach($this->bugtracker_model->getTypes() as $row ){
+                                    $array[] = $row->title;
+                                }
+                                echo form_dropdown('type_Bug',  $array , '', $classDrop);
+                            ?>
+                            <!-- dropdown -->
+                        </div>
+                    </div>
+
+                    <script src="<?= base_url(); ?>core/ckeditor_basic/ckeditor.js"></script>
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label uk-text-large"><?= $this->lang->line('form_description'); ?></label>
+                        <div class="uk-form-controls">
+                            <div class="uk-width-1-1">
+                                <?= form_textarea('bug_description', $this->config->item('textarea'), 'id="ckeditor"'); ?>
+                                <script>
+                                    CKEDITOR.replace('ckeditor');
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <div class="uk-form-controls">
+                            <div class="uk-inline uk-width-1-1">
+                                <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: link"></span>
+                                <?= form_input($url_form); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-modal-footer uk-text-right actions">
+                    <?= form_button('button_cancel', $this->lang->line('button_cancel'), $close_form); ?>
+                    <?= form_submit($submit_form); ?>
+                </div>
+            <?php form_close(); ?>
+        </div>
+    </div>
--- application/modules/bugtracker/views/post.php
+++ application/modules/bugtracker/views/post.php
@@ -16,13 +16,13 @@
 <?php if (isset($_POST['btn_closeBugtracker'])) {
     $this->bugtracker_model->closeIssue($idlink);
 } ?>
-<!DOCTYPE html>
-<html>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
     <title><?= $this->config->item('ProjectName'); ?> - <?= $this->lang->line('nav_bugtracker'); ?></title>
-    <script src="<?= base_url(); ?>assets/js/9013706011.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/blizzcms-general.css">
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/blizzcms-app.css">
@@ -41,10 +41,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- font-awesome End -->
 
-    <link href="https://api.dkamps18.net/css/font/discord/discord.css" rel="stylesheet"  type="text/css">
     <!-- custom footer -->
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <!-- custom footer -->
+    <!--[if lte IE 8]>
+        <script type="text/javascript" src="/<?= base_url(); ?>assets/js/jquery.min.js?v=88"></script>
+    <![endif]-->
 </head>
 
 <body class="en-us <?= $this->config->item('theme_name'); ?> glass-header preload" lang="en" data-locale="en-gb" data-device="desktop" data-name="index">
@@ -55,96 +57,97 @@
     </div>
     </div>
     <!-- submenu -->
-    <br><br><br>
-    <div role="main">
-        <section class="Scm-content">
-            <div class="section">
-                <h2 style="color: #fff;"><i class="fa fa-bookmark" aria-hidden="true"></i> <?= $this->bugtracker_model->getTitleIssue($idlink); ?></h2>
-                <p><?= $this->bugtracker_model->getDescIssue($idlink); ?></p>
-                <div class="uk-margin">
-                    <div class="uk-placeholder uk-text-center"><?= $this->bugtracker_model->getUrlIssue($idlink); ?></div>
-                </div>
-                <div class="uk-column-1-3 uk-column-divider">
-                    <p><i class="fa fa-list" aria-hidden="true"></i> <?= $this->lang->line('form_type'); ?>: <span class="uk-label"><?= $this->bugtracker_model->getType($this->bugtracker_model->getTypeID($idlink)); ?></span></p>
-                    <p><i class="fa fa-exclamation-circle" aria-hidden="true"></i> <?= $this->lang->line('column_priority'); ?>: <span class="uk-label uk-label-danger"><?= $this->bugtracker_model->getPriority($this->bugtracker_model->getPriorityID($idlink)); ?></span></p>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> <?= $this->lang->line('column_date'); ?>: <span class="uk-badge"><?= date('Y-m-d', $this->bugtracker_model->getDate($idlink)) ?></span></p>
-                </div>
-                <div class="uk-column-1-3 uk-column-divider">
-                    <p><i class="fa fa-tags" aria-hidden="true"></i> <?= $this->lang->line('column_status'); ?>: <span class="uk-label uk-label-success"><?= $this->bugtracker_model->getStatus($this->bugtracker_model->getStatusID($idlink)); ?></span></p>
-                    <p><i class="fa fa-info-circle" aria-hidden="true"></i> <?= $this->lang->line('column_status'); ?>:
-                        <?php if ($this->bugtracker_model->closeStatus($idlink) == '0') { ?>
-                            <span class="uk-label uk-label-success"><?= $this->lang->line('option_open'); ?></span>
-                        <?php } else { ?>
-                            <span class="uk-label uk-label-danger"><?= $this->lang->line('option_closed'); ?></span>
-                        <?php } ?>
-                    </p>
-                    <p><i class="fa fa-user-circle-o" aria-hidden="true"></i> <?= $this->lang->line('column_author'); ?>: <?= $this->m_data->getUsernameID($this->bugtracker_model->getAuthor($idlink)); ?></p>
-                </div>
-            </div>
-        </section>
-        <?php if($this->m_data->getRank($this->session->userdata('fx_sess_id')) > 0) { ?>
+    <div class="Page-container">
+        <div class="Page-content en-US">
             <section class="Scm-content">
+                <div class="space-adaptive-medium"></div>
                 <div class="section">
-                    <!-- buttons -->
-                    <div class="uk-column-1-3 uk-column-divider">
-                        <!-- priority -->
-                        <div>
-                            <div class="uk-margin">
-                                <form method="post" action="">
-                                    <div class="uk-form-controls">
-                                        <select class="uk-select uk-form-width-medium" id="form-stacked-select" name="prioryValue">
-                                            <?php foreach($this->bugtracker_model->getPriorityGeneral()->result() as $priory) { ?>
-                                                <option value="<?= $priory->id ?>"><?= $priory->title ?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                    <br>
-                                    <button class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="changePriory"><?= $this->lang->line('button_change'); ?></button>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- priority -->
-                        <!-- status -->
-                        <div>
-                            <div class="uk-margin">
-                                <form method="post" action="">
-                                    <div class="uk-form-controls">
-                                        <select class="uk-select uk-form-width-medium" id="form-stacked-select" name="StatusValue">
-                                            <?php foreach($this->bugtracker_model->getStatusGeneral()->result() as $priory) { ?>
-                                                <option value="<?= $priory->id ?>"><?= $priory->title ?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                    <br>
-                                    <button class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="changeStatus"><?= $this->lang->line('button_change'); ?></button>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- status -->
-                        <!-- type -->
-                        <div>
-                            <div class="uk-margin">
-                                <form method="post" action="">
-                                    <div class="uk-form-controls">
-                                        <select class="uk-select uk-form-width-medium" id="form-stacked-select" name="typesValue">
-                                            <?php foreach($this->bugtracker_model->getTypesGeneral()->result() as $priory) { ?>
-                                                <option value="<?= $priory->id ?>"><?= $priory->title ?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                    <br>
-                                    <button class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="changetypes"><?= $this->lang->line('button_change'); ?></button>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- type -->
+                    <h2 style="color: #fff;"><i class="fa fa-bookmark" aria-hidden="true"></i> <?= $this->bugtracker_model->getTitleIssue($idlink); ?></h2>
+                    <p><?= $this->bugtracker_model->getDescIssue($idlink); ?></p>
+                    <div class="uk-margin">
+                        <div class="uk-placeholder uk-text-center"><?= $this->bugtracker_model->getUrlIssue($idlink); ?></div>
                     </div>
-                    <!-- buttons -->
-                    <br>
-                    <form method="post" action="">
-                        <button type="submit" name="btn_closeBugtracker" class="uk-button uk-button-secondary uk-width-1-1"><?= $this->lang->line('button_close'); ?></button>
-                    </form>
+                    <div class="uk-column-1-3 uk-column-divider">
+                        <p><i class="fa fa-list" aria-hidden="true"></i> <?= $this->lang->line('form_type'); ?>: <span class="uk-label"><?= $this->bugtracker_model->getType($this->bugtracker_model->getTypeID($idlink)); ?></span></p>
+                        <p><i class="fa fa-exclamation-circle" aria-hidden="true"></i> <?= $this->lang->line('column_priority'); ?>: <span class="uk-label uk-label-danger"><?= $this->bugtracker_model->getPriority($this->bugtracker_model->getPriorityID($idlink)); ?></span></p>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i> <?= $this->lang->line('column_date'); ?>: <span class="uk-badge"><?= date('Y-m-d', $this->bugtracker_model->getDate($idlink)) ?></span></p>
+                    </div>
+                    <div class="uk-column-1-3 uk-column-divider">
+                        <p><i class="fa fa-tags" aria-hidden="true"></i> <?= $this->lang->line('column_status'); ?>: <span class="uk-label uk-label-success"><?= $this->bugtracker_model->getStatus($this->bugtracker_model->getStatusID($idlink)); ?></span></p>
+                        <p><i class="fa fa-info-circle" aria-hidden="true"></i> <?= $this->lang->line('column_status'); ?>:
+                            <?php if ($this->bugtracker_model->closeStatus($idlink) == '0') { ?>
+                                <span class="uk-label uk-label-success"><?= $this->lang->line('option_open'); ?></span>
+                            <?php } else { ?>
+                                <span class="uk-label uk-label-danger"><?= $this->lang->line('option_closed'); ?></span>
+                            <?php } ?>
+                        </p>
+                        <p><i class="fa fa-user-circle-o" aria-hidden="true"></i> <?= $this->lang->line('column_author'); ?>: <?= $this->m_data->getUsernameID($this->bugtracker_model->getAuthor($idlink)); ?></p>
+                    </div>
                 </div>
             </section>
-        <?php } ?>
-    </div>
+            <?php if($this->m_data->getRank($this->session->userdata('fx_sess_id')) > 0) { ?>
+                <section class="Scm-content">
+                    <div class="section">
+                        <!-- buttons -->
+                        <div class="uk-column-1-3 uk-column-divider">
+                            <!-- priority -->
+                            <div>
+                                <div class="uk-margin">
+                                    <form method="post" action="">
+                                        <div class="uk-form-controls">
+                                            <select class="uk-select uk-form-width-medium" id="form-stacked-select" name="prioryValue">
+                                                <?php foreach($this->bugtracker_model->getPriorityGeneral()->result() as $priory) { ?>
+                                                    <option value="<?= $priory->id ?>"><?= $priory->title ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                        <br>
+                                        <button class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="changePriory"><?= $this->lang->line('button_change'); ?></button>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- priority -->
+                            <!-- status -->
+                            <div>
+                                <div class="uk-margin">
+                                    <form method="post" action="">
+                                        <div class="uk-form-controls">
+                                            <select class="uk-select uk-form-width-medium" id="form-stacked-select" name="StatusValue">
+                                                <?php foreach($this->bugtracker_model->getStatusGeneral()->result() as $priory) { ?>
+                                                    <option value="<?= $priory->id ?>"><?= $priory->title ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                        <br>
+                                        <button class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="changeStatus"><?= $this->lang->line('button_change'); ?></button>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- status -->
+                            <!-- type -->
+                            <div>
+                                <div class="uk-margin">
+                                    <form method="post" action="">
+                                        <div class="uk-form-controls">
+                                            <select class="uk-select uk-form-width-medium" id="form-stacked-select" name="typesValue">
+                                                <?php foreach($this->bugtracker_model->getTypesGeneral()->result() as $priory) { ?>
+                                                    <option value="<?= $priory->id ?>"><?= $priory->title ?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                        <br>
+                                        <button class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="changetypes"><?= $this->lang->line('button_change'); ?></button>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- type -->
+                        </div>
+                        <!-- buttons -->
+                        <br>
+                        <form method="post" action="">
+                            <button type="submit" name="btn_closeBugtracker" class="uk-button uk-button-secondary uk-width-1-1"><?= $this->lang->line('button_close'); ?></button>
+                        </form>
+                    </div>
+                </section>
+            <?php } ?>
+        </div>
