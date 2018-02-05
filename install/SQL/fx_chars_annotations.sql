79391f518689168bfdfbe2b99b677da7aa782405
DZywolf <luis.wolf.core@gmail.com>
2018-02-05 05:11:41 -0300

    Fixed SQL

--- install/SQL/fx_chars_annotations.sql
+++ install/SQL/fx_chars_annotations.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.7.21-0ubuntu0.16.04.1 : Database - blizzcms
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,7 +13,6 @@ MySQL - 5.7.21-0ubuntu0.16.04.1 : Database - blizzcms
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`blizzcms` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 /*Table structure for table `fx_chars_annotations` */
 
