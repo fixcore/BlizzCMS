d34e6c661a8695fd23612984f5c0bdf927cca3e1
DZywolf <luis.wolf.core@gmail.com>
2018-02-06 23:49:07 -0300

    Add Thai Language

---
+++ application/language/thai/admin_lang.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * BlizzCMS
+ *
+ * An open source application based on CodeIgniter for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*Navigation Menu Lang*/
+$lang['admin_dashboard'] = 'แผงควบคุม';
+$lang['admin_users'] = 'ผู้ใช้งาน';
+$lang['admin_users_list'] = 'รายชื่อผู้ใช้งาน';
+$lang['admin_chars_list'] = 'รายชื่อตัวละคร';
+$lang['admin_website'] = 'เว็บไซต์';
+$lang['admin_news'] = 'ข่าว';
+$lang['admin_changelogs'] = 'การอัปเดทและแก้ไข';
+$lang['admin_pages'] = 'หน้าเพจใหม่';
+$lang['admin_store'] = 'ร้านค้า';
+$lang['admin_items'] = 'จัดการไอเทม';
+$lang['admin_forums'] = 'ฟอรั่ม';
+$lang['admin_manage_categories'] = 'จัดการหมวดหมู่';
+$lang['admin_manege_forums'] = 'จัดการฟอรั่ม';
+
+/*Count Lang*/
+$lang['players_count'] = 'ผู้เล่นออนไลน์';
+$lang['account_count'] = 'บัญชีทั้งหมด';
+$lang['staff_count'] = 'บัญชี STAFF';
+$lang['ban_count'] = 'บัญชีที่ถูกแบน';
+
+/*Button Lang*/
+$lang['button_server_restart'] = 'รีสตาร์ทเซิร์ฟเวอร์';
+$lang['button_unban'] = 'ปลดแบน';
+$lang['button_ban'] = 'แบน';
+$lang['button_re_grant_account'] = 'ลบจากการเป็นจีเอ็ม';
+$lang['button_grant_account'] = 'ตั้งให้เป็นจีเอ็ม';
+$lang['button_grant_web_acc'] = 'ตั้งให้เป็นแอดมิน';
+$lang['button_re_grant_web_acc'] = 'ลบจากการเป็นแอดมิน';
+$lang['button_change_level'] = 'แก้ไขเลเวล';
+
+/*Panel Lang*/
+$lang['panel_admin_user_manage'] = 'จัดการผู้ใช้งาน';
+$lang['panel_admin_unban_account'] = 'ปลดแบนบัญชี';
+$lang['panel_admin_ban_account'] = 'แบนบัญช';
+$lang['panel_admin_rank_account'] = 'ระดับจีเอ็ม';
+$lang['panel_admin_web_rank'] = 'ระดับบนเว็บ';
+$lang['panel_admin_general_info'] = 'ข้อมูลทั่วไป';
+$lang['panel_admin_annotations'] = 'คำอธิบายประกอบ';
+$lang['panel_admin_mov_forum'] = 'การเปลี่ยนแปลงในฟอรั่ม';
+$lang['panel_admin_last_comments'] = 'ความคิดเห็นล่าสุด';
+$lang['panel_admin_char_manage'] = 'จัดการตัวละคร';
+$lang['panel_admin_change_level'] = 'แก้ไขเลเวล';
+$lang['panel_admin_rename'] = 'แก้ไขชื่อ';
+$lang['panel_admin_unban_char'] = 'ปลดแบนตัวละคร';
+$lang['panel_admin_ban_char'] = 'แบนตัวละคร';
+$lang['panel_admin_customize'] = 'ปรับแต่งตัวละคร';
+$lang['panel_admin_change_race'] = 'แก้ไขการแข่งขัน';
+$lang['panel_admin_change_faction'] = 'แก้ไขฝ่าย';
+$lang['panel_admin_changelogs_list'] = 'รายการอัปเดทและแก้ไข';
+$lang['panel_admin_pages_list'] = 'รายการหน้าเพจใหม่';
+$lang['panel_admin_news_list'] = 'รายการข่าว';
+$lang['panel_admin_item_list'] = 'รายการไอเทม';
+$lang['panel_admin_edit_news'] = 'แก้ไขข่าว';
+
+/*Column Lang*/
+$lang['column_race'] = 'Race';
+$lang['column_class'] = 'อาชีพ';
+$lang['column_level'] = 'เลเวล';
+$lang['column_money'] = 'เงิน';
+$lang['column_own'] = 'เจ้าของ';
+$lang['column_action'] = 'Action';
+
+/*Form Lang*/
+$lang['form_create_changelog'] = 'สร้างรายการอัปเดทและแก้ไข';
+$lang['form_changelog_title'] = 'หัวข้อการอัปเดทและแก้ไข';
+$lang['form_create_pages'] = 'สร้างรายการหน้าเพจใหม่';
+$lang['form_create_news'] = 'สร้างรายการข่าว';
+$lang['form_news_title'] = 'หัวข้อข่าว';
+$lang['form_upload_file'] = 'อัปโหลดไฟล์';
+$lang['form_create_category'] = 'สร้างหมวดหมู่';
+$lang['form_create_forums'] = 'สร้างฟอรั่ม';
+$lang['form_category_title'] = 'หัวข้อหมวดหมู่';
+$lang['form_forum_title'] = 'หัวข้อฟอรั่ม';
+$lang['form_forum_description'] = 'ป้อนคำอธิบายโดยย่อของฟอรัม';
+$lang['form_forum_icon_name'] = 'ชื่อไอคอน';
+$lang['form_category'] = 'หมวดหมู่';
+$lang['form_create_item'] = 'สร้างไอเทม';
+$lang['form_store_item_name'] = 'ชื่อไอเทม';
+$lang['form_store_item_id'] = 'ไอดีไอเทม';
+$lang['form_store_image_name'] = 'ชื่อไฟล์รูปภาพ';
+
+/*Placeholder Lang*/
+$lang['panel_admin_reason'] = 'เหตุผล';
+$lang['panel_admin_gmlevel'] = 'เลเวลจีเอ็ม';
+$lang['panel_admin_new_page_url'] = 'ลิ้งค์หน้าเว็บใหม่ของคุณคือ';
+
+/*Status Lang*/
+$lang['status_is_online'] = 'Player is Online, please disconnect';
+$lang['status_name_exist'] = 'This name already exist';
+
+/*Options Lang*/
+$lang['option_yes'] = 'Yes';
+$lang['option_no'] = 'No';
+$lang['option_everyone'] = 'Everyone';
+$lang['option_staff'] = 'STAFF';
+$lang['option_all'] = 'STAFF - Everyone';
+$lang['option_item'] = 'Item';
+
+/*Options Lang*/
+$lang['is_banned_reason'] = 'Is banned, reason:';
---
+++ application/language/thai/notification_lang.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * BlizzCMS
+ *
+ * An open source application based on CodeIgniter for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*Soap Lang*/
+$lang['store_senditem_subject'] = 'ร้านค้าออนไลน์';
+$lang['store_senditem_text'] = 'ขอขอบคุณสำหรับการซื้อไอเทม นี่คือไอเทมที่คุณได้ทำการซื้อ';
+
+/*Error Lang*/
+$lang['account_error'] = 'Invalid account or password';
+$lang['password_error'] = 'Invalid password';
+$lang['captcha_error'] = 'Please check the captcha';
+
+/*Message Lang*/
+$lang['store_success'] = 'ซื้อไอเทมสำเร็จแล้ว';
+$lang['account_already_exist'] = 'This account already exists';
+$lang['password_not_match'] = 'Passwords do not match';
+$lang['password_same'] = 'The password is the same.';
+$lang['opassword_not_match'] = 'Old Password do not match';
+$lang['oemail_not_match'] = 'The current email is not correct';
+$lang['email_used'] = 'Email in use';
+$lang['expansion_not_found'] = 'Expansion not found.';
+$lang['image_upload_error'] = 'Your image must be in .jpg format';
+$lang['changelog_not_found'] = 'The server does not have changelogs to inform at this time';
+
+$lang['char_newlevel'] = 'Receive a new level';
+$lang['char_oldlevel'] = 'Before it was';
+$lang['char_newname'] = 'It has a new name';
+$lang['char_oldname'] = 'Before it was';
+$lang['unbanned'] = 'Unbanned';
+$lang['char_customAction'] = 'Get a customization';
+$lang['char_chanraceAction'] = 'Get a Race Change';
+$lang['char_chanfactAction'] = 'Get a Faction Change';
+$lang['was_ban'] = 'Was banned';
+$lang['receive_gmAnno'] = 'Received GM rank';
+$lang['remove_gmAnnotation'] = 'The GM rank was removed';
+$lang['receive_addmAnnoW'] = 'Received ADM Web rank';
+$lang['remove_addmAnnoW'] = 'The ADM Web rank was removed';
+$lang['buy_frist'] = 'You must first add items to your car.';
+$lang['points_insuff'] = 'Insufficient points';
+$lang['shopping_cart'] = 'Shopping Cart';
+$lang['store_see'] = 'See the Store';
+$lang['no_players'] = 'No players Online';
+$lang['users_on'] = 'Users Online';
