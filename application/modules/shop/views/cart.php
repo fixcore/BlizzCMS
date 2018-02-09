bbc9205b0af15b18fdb1f5aef55696f3e783f070
DZywolf <luis.wolf.core@gmail.com>
2018-02-09 16:02:45 -0300

    Remove

--- application/modules/shop/views/cart.php
+++ application/modules/shop/views/cart.php
@@ -28,40 +28,6 @@
     <script src="<?= base_url(); ?>core/js/jquery-3.3.1.min.js"></script>
 </head>
 
-<!DOCTYPE html>
-<html>
-<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
-<head>
-    <title><?= $this->config->item('ProjectName'); ?> - <?= $this->lang->line('cart'); ?></title>
-    <script src="<?= base_url(); ?>core/js/9013706011.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/blizzcms-general.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/blizzcms-app.css">
-    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url('assets/css/blizzcms-template.css') ?>"/>
-    <link rel="stylesheet" type="text/css" media="all" href="<?= base_url('theme/'); ?><?= $this->config->item('theme_name'); ?>/css/<?= $this->config->item('theme_name'); ?>.css"/>
-    <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/images/favicon.ico">
-    <!-- UiKit Start -->
-    <!-- UIkit CSS -->
-    <link rel="stylesheet" href="<?= base_url(); ?>core/uikit/css/uikit.min.css" />
-
-    <!-- UIkit JS -->
-    <script src="<?= base_url(); ?>core/uikit/js/uikit.min.js"></script>
-    <script src="<?= base_url(); ?>core/uikit/js/uikit-icons.min.js"></script>
-    <!-- UiKit end -->
-    <!-- font-awesome Start -->
-    <link rel="stylesheet" href="<?= base_url(); ?>core/font-awesome/css/font-awesome.min.css">
-    <!-- font-awesome End -->
-
-    <!-- Wowhead START -->
-    <script>var whTooltips = {colorLinks: true, iconizeLinks: false, renameLinks: false};</script>
-    <script type="text/javascript" src="//wow.zamimg.com/widgets/power.js"></script>
-    <!-- Wowhead START -->
-    <!-- custom footer -->
-    <script src="<?= base_url(); ?>core/js/jquery-3.3.1.min.js"></script>
-    <!-- custom footer -->
-</head>
-
 <body class="en-us <?= $this->config->item('theme_name'); ?> glass-header preload" lang="en" data-locale="en-gb" data-device="desktop" data-name="index">
     <!-- header -->
     <?php $this->load->view('general/icons'); ?>
