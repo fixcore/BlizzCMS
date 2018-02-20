cf5571b70206130ade0062ab869f33b3957fa1fb
Vipo <develop.km14@gmail.com>
2018-02-20 02:36:42 -0500

    jquery

--- application/modules/admin/views/general/footer.php
+++ application/modules/admin/views/general/footer.php
@@ -1,6 +1,3 @@
-    <!-- JQuery -->
-    <script src="<?= base_url(); ?>core/js/jquery-3.3.1.min.js"></script>
-    <script src="<?= base_url(); ?>core/js/jquery.transit.min.js"></script>
     <!-- BlizzCMS -->
     <script src="<?= base_url(); ?>core/js/blizzcms-core.js"></script>
 </body>
