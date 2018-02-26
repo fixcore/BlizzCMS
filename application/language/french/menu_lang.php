a934e3f055e31908f498915c5cc6437808ae61ca
Xtoons <dev@rebornspartan.net>
2018-02-26 10:57:20 +0100

    Lang/French: Translate EN to FR
    menu_lang.php and notification_lang.php added to translate 100%

--- application/language/french/menu_lang.php
+++ application/language/french/menu_lang.php
@@ -30,13 +30,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*Navigation Menu*/
 $lang['nav_menu'] = 'Menu';
-$lang['nav_more'] = 'More';
-$lang['nav_news'] = 'News';
+$lang['nav_more'] = 'Plus';
+$lang['nav_news'] = 'Nouveautés';
 $lang['nav_forums'] = 'Forums';
-$lang['nav_store'] = 'Store';
+$lang['nav_store'] = 'Boutique';
 $lang['nav_bugtracker'] = 'Bugtracker';
 $lang['nav_changelogs'] = 'Changelogs';
-$lang['nav_pvp_statistics'] = 'PvP Statistics';
-$lang['nav_arena_statistics'] = 'Arena Statistics';
+$lang['nav_pvp_statistics'] = 'Statistiques PvP';
+$lang['nav_arena_statistics'] = 'Statistiques Arène';
 
-$lang['nav_account'] = 'My Account';
+$lang['nav_account'] = 'Mon compte';
